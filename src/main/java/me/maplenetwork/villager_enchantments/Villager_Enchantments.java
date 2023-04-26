3b96ded3c5071a30ff83f0fca1471bb1668e39cb
woliver99 <oliver_giroux@hotmail.com>
2023-04-26 01:11:12 -0400

    1.0.0
    Fully working with support to 1.17

--- src/main/java/me/maplenetwork/villager_enchantments/Villager_Enchantments.java
+++ src/main/java/me/maplenetwork/villager_enchantments/Villager_Enchantments.java
@@ -1,10 +1,12 @@
 package me.maplenetwork.villager_enchantments;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.*;
 import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Villager_Enchantments extends JavaPlugin {
+    static String minecraftVersion = Bukkit.getVersion().split("MC: ")[1].substring(0, Bukkit.getVersion().split("MC: ")[1].indexOf(')'));
 
     @Override
     public void onEnable() {
